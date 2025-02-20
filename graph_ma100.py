bbab85e698231a11cc06cdbe138a71ef29100281
Ludvig Viktor Lindmark <hsm508@alumni.ku.dk>
2025-02-20 10:41:27 +0100

    tilføjelse af ma100_1

--- graph_ma100.py
+++ graph_ma100.py
@@ -9,26 +9,38 @@ from os.path import basename
 
 def main():
     # Start
-    times = 25
+    market = SIMULATOR.setup(config.defaultconfig)
+    nash = np.mean(market.get_nash_profits())
+    mono = np.mean(market.get_monopoly_profits())
+
+    times = 100
     maxit = 1500000
 
     average_collusion_quotient = []
     new_config = config.create_config(iterations=maxit)
     sum_collusion_quotients = 0
-    collusion_quotient_list = []
+    
     sim_start = time.time()
+    ma100 = np.zeros((maxit-99))
+    #change ma100 to a np.array() to avoid the error
+    ma100 = None
+
+
     for i in range(times):
 
         market, states = SIMULATOR.simulate(new_config)
         profits = np.array([state.profits for state in states])
         profits = np.mean(profits, axis=1)
-        nash = np.mean(market.get_nash_profits())
-        mono = np.mean(market.get_monopoly_profits())
-
+        collusion_quotient_list = []
         for i in range(maxit):
             collusion_quotient = lib.get_collusion_quotient(profits[i], nash, mono)
             collusion_quotient_list.append(collusion_quotient)
-        ma100 += np.array(lib.moving_average(collusion_quotient_list, 100))
+
+        print((lib.moving_average(collusion_quotient_list, 100)/times).shape)
+        if ma100 is None:
+            ma100 = lib.moving_average(collusion_quotient_list, 100)/times
+        else:
+            ma100 += lib.moving_average(collusion_quotient_list, 100)/times
 
     #linear space length of ma100
     repetitions = np.linspace(0, len(ma100), len(ma100))
