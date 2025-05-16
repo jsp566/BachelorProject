da30c2886d162b544cba55a8f4f220e24af0d5f3
jsp566 <antonni9801@gmail.com>
2025-05-16 22:28:38 +0200

    ASSYMMETRIC

--- graph_default_assymetric_no_convergence.py
+++ graph_default_assymetric_no_convergence.py
@@ -28,48 +28,28 @@ def main():
 
     SIMULATOR.simulate_sessions(new_config, filename=filename, parallel=parallel, savedData=savedData)
     
+    true_nash_cq = lib.get_collusion_quotient(market.get_true_nash_profits(), market.get_nash_profits(), market.get_monopoly_profits())
+    true_nash_prices = market.get_true_nash_prices()
+    monopoly_prices = market.get_monopoly_prices()
 
     # Collusion Quotient:
-    lengths = []
-    collusion_quotients = []
+    
+    collusion_quotients = np.zeros((iterations+1, 3))
+    prices = np.zeros((iterations+1, 3))
     for i in range(sessions):
         with open(os.path.join(os.getcwd(), 'Output', 'Data', filename, str(i) + ".pkl"), 'rb') as f:
             result = pickle.load(f)
-        lengths.append(len(result))
-        collusion_quotients.append(np.mean([state.collussion_quotient  for state in result], axis=1))
-   
 
-    plt.hist(lengths, bins=100)
-    plt.xlabel('Length of session')
-    plt.ylabel('Frequency')
-    plt.title('Length of session')
-    plt.savefig(config.create_filepath(filename + "_lengths"))
+        collusion_quotients += np.array([state.collussion_quotient for state in result]) / sessions
+        prices += np.array([state.prices for state in result]) / sessions
 
-    plt.clf()
+    # transpose collusion quotient shape
+    collusion_quotients = np.transpose(collusion_quotients)
+    prices = np.transpose(prices)
 
-    min_length = min(lengths)
-    max_length = max(lengths)
-
-
-    for i in range(sessions):
-        cq = np.array(collusion_quotients[i])
-        if len(cq) < max_length:
-            collusion_quotients[i] = np.append(cq, [np.mean(cq[-100:])]*(max_length - len(cq)))
-
-
-    collusion_quotients = np.array(collusion_quotients)
-
-    average_collusion_quotient = np.mean(collusion_quotients, axis=0)
-
-    plt.plot(range(min_length), average_collusion_quotient[:min_length])
-    plt.ylabel('Collusion Quotient')
-    plt.xlabel('Period')
-    plt.savefig(config.create_filepath(filename + "_collusion_quotient"))
-
-    plt.clf()
-
-    period = range(len(average_collusion_quotient))
-    plt.plot(period, average_collusion_quotient)
+    period = range(iterations+1)
+    plt.plot(period, np.mean(collusion_quotients, axis=0))
+    plt.axhline(y=np.mean(true_nash_cq), color='r', linestyle='--', label='True Nash CQ')
     plt.ylabel('Collusion Quotient')
     plt.xlabel('Period')
     plt.savefig(config.create_filepath(filename + "_collusion_quotient_full"))
@@ -77,16 +57,118 @@ def main():
     plt.clf()
 
     # Moving average
-    ma100=  lib.moving_average(average_collusion_quotient, 100)
+    ma100=  lib.moving_average(np.mean(collusion_quotients, axis=0), 100)
     repetitions = np.linspace(0, len(ma100), len(ma100))
 
     plt.plot(repetitions, ma100)
+    plt.axhline(y=np.mean(true_nash_cq), color='r', linestyle='--', label='True Nash CQ')
     plt.ylabel('Collusion Quotient')
     plt.xlabel('Period')
     
 
-    plt.savefig(config.create_filepath(filename + "_ma100"))
+    plt.savefig(config.create_filepath(filename + "_ma100_full"))
+    plt.clf()
 
+
+    plt.plot(period, collusion_quotients[0], label='Product 1')
+    plt.plot(period, collusion_quotients[1], label='Product 2')
+    plt.plot(period, collusion_quotients[2], label='Product 3')
+    plt.axhline(y=true_nash_cq[0], color='r', linestyle='--', label='True Nash CQ Product 1')
+    plt.axhline(y=true_nash_cq[1], color='g', linestyle='--', label='True Nash CQ Product 2')
+    plt.axhline(y=true_nash_cq[2], color='b', linestyle='--', label='True Nash CQ Product 3')
+    plt.legend()
+    plt.ylabel('Collusion Quotient')
+    plt.xlabel('Period')
+    plt.savefig(config.create_filepath(filename + "_collusion_quotient_products"))
+
+    plt.clf()
+
+    # Moving average
+    ma100=  lib.moving_average(collusion_quotients[0], 100)
+    repetitions = np.linspace(0, len(ma100), len(ma100))
+    plt.plot(repetitions, ma100, label='Product 1')
+    ma100=  lib.moving_average(collusion_quotients[1], 100)
+    plt.plot(repetitions, ma100, label='Product 2')
+    ma100=  lib.moving_average(collusion_quotients[2], 100)
+    plt.plot(repetitions, ma100, label='Product 3')
+
+    plt.axhline(y=true_nash_cq[0], color='r', linestyle='--', label='True Nash CQ Product 1')
+    plt.axhline(y=true_nash_cq[1], color='g', linestyle='--', label='True Nash CQ Product 2')
+    plt.axhline(y=true_nash_cq[2], color='b', linestyle='--', label='True Nash CQ Product 3')
+    plt.ylabel('Collusion Quotient')
+    plt.xlabel('Period')
+    
+
+    plt.savefig(config.create_filepath(filename + "_ma100_products"))
+    plt.clf()
+
+
+    # Plotting prices
+    period = range(iterations+1)
+    plt.plot(period, np.mean(prices, axis=0))
+    plt.axhline(y=np.mean(true_nash_prices), color='r', linestyle='--', label='True Nash Prices')
+    plt.axhline(y=np.mean(monopoly_prices), color='g', linestyle='--', label='Monopoly Prices')
+
+    plt.ylabel('Prices')
+    plt.xlabel('Period')
+    plt.savefig(config.create_filepath(filename + "_prices_full"))
+
+    plt.clf()
+
+    # Moving average
+    ma100=  lib.moving_average(np.mean(prices, axis=0), 100)
+    repetitions = np.linspace(0, len(ma100), len(ma100))
+
+    plt.plot(repetitions, ma100)
+    plt.axhline(y=np.mean(true_nash_prices), color='r', linestyle='--', label='True Nash Prices')
+    plt.axhline(y=np.mean(monopoly_prices), color='g', linestyle='--', label='Monopoly Prices')
+    plt.ylabel('Prices')
+    plt.xlabel('Period')
+    
+
+    plt.savefig(config.create_filepath(filename + "_ma100_prices_full"))
+    plt.clf()
+
+
+    plt.plot(period, prices[0], label='Product 1')
+    plt.plot(period, prices[1], label='Product 2')
+    plt.plot(period, prices[2], label='Product 3')
+    plt.axhline(y=true_nash_prices[0], color='r', linestyle='--', label='True Nash Prices Product 1')
+    plt.axhline(y=true_nash_prices[1], color='g', linestyle='--', label='True Nash Prices Product 2')
+    plt.axhline(y=true_nash_prices[2], color='b', linestyle='--', label='True Nash Prices Product 3')
+    plt.axhline(y=monopoly_prices[0], color='g', linestyle='--', label='Monopoly Prices Product 1')
+    plt.axhline(y=monopoly_prices[1], color='g', linestyle='--', label='Monopoly Prices Product 2')
+    plt.axhline(y=monopoly_prices[2], color='g', linestyle='--', label='Monopoly Prices Product 3')
+
+    plt.legend()
+    plt.ylabel('Prices')
+    plt.xlabel('Period')
+    plt.savefig(config.create_filepath(filename + "_prices_products"))
+
+    plt.clf()
+
+    # Moving average
+    ma100=  lib.moving_average(prices[0], 100)
+    repetitions = np.linspace(0, len(ma100), len(ma100))
+    plt.plot(repetitions, ma100, label='Product 1')
+    ma100=  lib.moving_average(prices[1], 100)
+    plt.plot(repetitions, ma100, label='Product 2')
+    ma100=  lib.moving_average(prices[2], 100)
+    plt.plot(repetitions, ma100, label='Product 3')
+
+    plt.axhline(y=true_nash_prices[0], color='r', linestyle='--', label='True Nash Prices Product 1')
+    plt.axhline(y=true_nash_prices[1], color='g', linestyle='--', label='True Nash Prices Product 2')
+    plt.axhline(y=true_nash_prices[2], color='b', linestyle='--', label='True Nash Prices Product 3')
+    plt.axhline(y=monopoly_prices[0], color='g', linestyle='--', label='Monopoly Prices Product 1')
+    plt.axhline(y=monopoly_prices[1], color='g', linestyle='--', label='Monopoly Prices Product 2')
+    plt.axhline(y=monopoly_prices[2], color='g', linestyle='--', label='Monopoly Prices Product 3')
+
+    plt.ylabel('Prices')
+    plt.xlabel('Period')
+    
+
+    plt.savefig(config.create_filepath(filename + "_ma100_prices_products"))
+    plt.clf()
 
 
 
