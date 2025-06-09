03b104345e7eabfd214a3707f0739f1109aed833
Ludvig Viktor Lindmark <hsm508@alumni.ku.dk>
2025-06-09 21:27:30 +0200

    sista

--- graph_beta.py
+++ graph_beta.py
@@ -14,12 +14,12 @@ filename = basename(__file__).replace('.py', '')
 
 def main():
     # Start
-
-    betas = np.linspace(2*10**-7, 4*10**-6, 4, endpoint=True) #SET Beta values
+    alpha = 0.15
+    betas = np.linspace(4*10**-7, 4*10**-6, 4, endpoint=False) #SET Beta values
     sessions = 100
     iterations = 10**8
-    numb_firms = 2
-    parallel=False
+    numb_firms = 3
+    parallel=True
     savedData = True
     
     new_config = config.create_config(sessions=sessions, iterations=iterations, numb_firms=numb_firms)
@@ -28,30 +28,25 @@ def main():
         "exploration_rate_params": [(beta,) for beta in betas]
     }
 
-    SIMULATOR.simulate_sessions(new_config, filename=filename, parallel=parallel, savedData=savedData, variations=variations, learning_rate=0.1)
-
-    average_collusion_quotient = [] #List to store average collusion quotients
+    SIMULATOR.simulate_sessions(new_config, filename=filename, parallel=parallel, savedData=savedData, variations=variations)
+    collusion_quotients_list = []
+    lengths_list = []
+    ticks = []
     for beta in betas:
         collusion_quotients = []
         lengths = []
-for i in range(sessions):
-            try:
-                with open(os.path.join(output_dir, new_filename, str(i) + ".pkl"), 'rb') as f:
-                    result = pickle.load(f)
-            except:
-                file_exists = False
-                break
+        for i in range(sessions):
+            with open(os.path.join(os.getcwd(), 'Output', 'Data', filename, f"(('exploration_rate_params', ({beta},)),)_" + str(i) + ".pkl"), 'rb') as f:
+                result = pickle.load(f)
             collusion_quotients.append(np.mean([state.collussion_quotient  for state in result[-100000:]]))
             lengths.append(len(result))
 
-
         collusion_quotients = np.transpose(collusion_quotients)
         lengths = np.transpose(lengths)
-        ticks.append(str(firms) + " firms")
+        ticks.append(str(beta))
         collusion_quotients_list.append(collusion_quotients)
         lengths_list.append(lengths)
 
-        firms += 1
 
     save_dir = os.path.join(os.getcwd(), 'Output', 'Graphs', filename)
     os.makedirs(save_dir, exist_ok=True)
@@ -59,7 +54,7 @@ for i in range(sessions):
     plt.boxplot(collusion_quotients_list, showmeans=True)
     plt.xticks(range(1, len(ticks) + 1), ticks)
     plt.ylabel('Collusion Quotient')
-    plt.xlabel('Number of firms')
+    plt.xlabel('Value of Beta')
     plt.subplots_adjust(left=0.2, right=0.8, top=0.95)
     plt.savefig(os.path.join(save_dir, filename + "_collusion_quotients.png"))
     plt.clf()
@@ -76,4 +71,3 @@ for i in range(sessions):
     
 if __name__ == "__main__":
     config.profile_main(main,filename)
-    
